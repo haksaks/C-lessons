c087ccf4e3a7249f2916a5b7e61a37ba49d1d3c7
haksaks <angel.k.staev.2022@elsys-bg.org>
2024-02-11 17:02:02 +0200

    printf changed

--- 11-typecasting-bitwise-operators/main.c
+++ 11-typecasting-bitwise-operators/main.c
@@ -7,12 +7,12 @@ int main(void){
     double b=5;
     double c=-24;
     struct QuadraticRootsResult x=findroots(a, b, c);
+    printf("The roots of the quadratic equation (%lf)x2 + (%lf)x + (%lf) are:\n", a, b, c);
     if(x.norealroots=='1'){
         printf("No real roots\n");
     }else{
         printf("x1 = %lf\n", x.x1);
         printf("x2 = %lf\n", x.x2);
     }
-
     return 0;
 }
