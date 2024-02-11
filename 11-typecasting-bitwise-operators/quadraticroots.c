dfb8e0bbe499b1ddc2d2f8d6e088428fabad0b7d
haksaks <angel.k.staev.2022@elsys-bg.org>
2024-02-11 16:27:35 +0200

    removed printf

--- 11-typecasting-bitwise-operators/quadraticroots.c
+++ 11-typecasting-bitwise-operators/quadraticroots.c
@@ -1,15 +1,12 @@
 #include "quadraticroots.h"
 #include <math.h>
-#include <stdio.h>
 
 struct QuadraticRootsResult roots;
 
 struct QuadraticRootsResult findroots(double a, double b, double c){
     double D=(b*b)-(4*a*c);
-    printf("%lf\n", D);
     if(D>0){
         roots.x1=-b+sqrtl(D);
-        printf("%lf\n", roots.x1);
         roots.x1=roots.x1/(a*2);
         roots.x2=-b-sqrtl(D);
         roots.x2=roots.x2/(a*2);
